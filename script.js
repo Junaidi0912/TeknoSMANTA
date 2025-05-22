f17c4e0d6bcaaa16137f1bc8616194bc500c3baf
Junaidi0912 <akhmadjunaidialfaridz1@gmail.com>
2025-05-22 18:59:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
   e.preventDefault();
 
-  // Credentials yang sudah ditentukan
-  const correctUsername = "PENH";
-  const correctPassword = "22102008";
 
-  // Ambil nilai input
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const correctUsername = "PENH";       
+  const correctPassword = "22102008";    
+
+  
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
   const errorMessage = document.getElementById('errorMessage');
 
-  // Validasi
+
   if (username === correctUsername && password === correctPassword) {
     alert("Login berhasil! Selamat datang, Admin.");
-    window.location.href = "https://junaidi0912.github.io/TeknoSMANTA/beranda";
+    window.location.href = "https://junaidi0912.github.io/TeknoSMANTA/beranda ";
   } else {
     errorMessage.style.display = 'block';
   }
